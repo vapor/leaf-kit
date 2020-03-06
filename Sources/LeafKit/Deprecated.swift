0b3896f2fa4e28741d9f1f77cf068acc5173dbe3
tanner0101 <me@tanner.xyz>
2020-03-05 19:46:31 -0500

    fix property

--- Sources/LeafKit/Deprecated.swift
+++ Sources/LeafKit/Deprecated.swift
@@ -1,5 +1,13 @@
 extension LeafRenderer {
     @available(*, deprecated, message: "Use files instead of fileio")
+    public var fileio: NonBlockingFileIO {
+        guard let nio = self.files as? NIOLeafFiles else {
+            fatalError("Unexpected non-NIO leaf files.")
+        }
+        return nio.fileio
+    }
+
+    @available(*, deprecated, message: "Use files instead of fileio")
     public convenience init(
         configuration: LeafConfiguration,
         cache: LeafCache = DefaultLeafCache(),
