d22a70d3a40a03290affc90445b61274904b1c91
Teague Clare <teague@objectbyconcept.com>
2020-04-17 15:35:39 -0400

    * Revert adding custom loop tags

--- Sources/LeafKit/LeafTag.swift
+++ Sources/LeafKit/LeafTag.swift
@@ -3,10 +3,7 @@ public protocol LeafTag {
 }
 
 public var defaultTags: [String: LeafTag] = [
-    "lowercased": Lowercased(),
-    "index": LoopIndex(),
-    "isFirst": LoopIsFirst(),
-    "isLast": LoopIsLast()
+    "lowercased": Lowercased()
 ]
 
 struct Lowercased: LeafTag {
@@ -15,26 +12,5 @@ struct Lowercased: LeafTag {
             throw "unable to lowercase unexpected data"
         }
         return .init(.string(str.lowercased()))
-    }
-}
-
-struct LoopIndex: LeafTag {
-    func render(_ ctx: LeafContext) throws -> LeafData {
-        guard let index = ctx.data["index"] else { throw "Loop accessor called on non-loop" }
-        return index
-    }
-}
-
-struct LoopIsFirst: LeafTag {
-    func render(_ ctx: LeafContext) throws -> LeafData {
-        guard let isFirst = ctx.data["isFirst"] else { throw "Loop accessor called on non-loop" }
-        return isFirst
-    }
-}
-
-struct LoopIsLast: LeafTag {
-    func render(_ ctx: LeafContext) throws -> LeafData {
-        guard let isLast = ctx.data["isLast"] else { throw "Loop accessor called on non-loop" }
-        return isLast
     }
 }
