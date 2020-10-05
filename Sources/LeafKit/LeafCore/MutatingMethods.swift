5270d93c0230792679317d660d34efe72a54e799
Teague Clare <teague@objectbyconcept.com>
2020-10-05 16:36:50 -0400

    - Add Array.popLast() & .append()

--- Sources/LeafKit/LeafCore/MutatingMethods.swift
+++ Sources/LeafKit/LeafCore/MutatingMethods.swift
@@ -1,7 +1,9 @@
 internal extension LeafEntities {
     func registerMutatingMethods() {
-        use(MutatingStrStrMap.append     , asMethod: "append")
-        use(MutatingStrToStrMap.popLast  , asMethod: "popLast")
+        use(MutatingStrStrMap.append      , asMethod: "append")
+        use(MutatingStrToStrMap.popLast   , asMethod: "popLast")
+        use(MutatingArrayAnyMap.append      , asMethod: "append")
+        use(MutatingArrayToAnyMap.popLast , asMethod: "popLast")
     }
 }
 /// Mutating (String, String)
@@ -20,7 +22,7 @@ internal struct MutatingStrStrMap: LeafMutatingMethod, StringStringParam, VoidRe
 }
 
 
-/// Mutating (String, Int) -> String
+/// Mutating (String) -> String
 internal struct MutatingStrToStrMap: LeafMutatingMethod, StringParam, StringReturn {
     func mutatingEvaluate(_ params: LeafCallValues) -> (mutate: LKData?, result: LKData) {
         let cache = params[0].string!
@@ -33,4 +35,38 @@ internal struct MutatingStrToStrMap: LeafMutatingMethod, StringParam, StringRetu
     
     private init(_ map: @escaping (inout String) -> String?) { f = map }
     private let f: (inout String) -> String?
+}
+
+
+/// Mutating (Array) -> Any
+internal struct MutatingArrayToAnyMap: LeafMutatingMethod, ArrayParam, AnyReturn {
+    func mutatingEvaluate(_ params: LeafCallValues) -> (mutate: LKData?, result: LKData) {
+        let cache = params[0].array!
+        var operand = cache
+        let result = f(&operand)
+        return (operand != cache ? .array(operand) : nil,
+                result != nil ? result! : .trueNil)
+    }
+    
+    static let popLast: Self = .init({$0.popLast()})
+    
+    private init(_ map: @escaping (inout [LeafData]) -> LeafData?) { f = map }
+    private let f: (inout [LeafData]) -> LeafData?
+}
+
+/// Mutating (Array, Any)
+internal struct MutatingArrayAnyMap: LeafMutatingMethod, VoidReturn {
+    static var callSignature: [LeafCallParameter] { [.array, .any] }
+    
+    func mutatingEvaluate(_ params: LeafCallValues) -> (mutate: LKData?, result: LKData) {
+        let cache = params[0].array!
+        var operand = cache
+        f(&operand, params[1])
+        return (operand != cache ? .array(operand) : nil, .trueNil)
+    }
+    
+    static let append: Self = .init({$0.append($1)})
+    
+    private init(_ map: @escaping (inout [LeafData], LeafData) -> ()) { f = map }
+    private let f: (inout [LeafData], LeafData) -> ()
 }
