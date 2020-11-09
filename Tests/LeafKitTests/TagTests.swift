07e60ff632dc087bead6a20e7242822bebbb01f7
Tim <0xtimc@gmail.com>
2020-11-09 14:32:31 +0000

    Backfill last tests

--- Tests/LeafKitTests/TagTests.swift
+++ Tests/LeafKitTests/TagTests.swift
@@ -52,4 +52,39 @@ class TagTests: XCTestCase {
         """
         try XCTAssertEqual(render(template, ["people": ["Tanner", "Logan", "Gwynne", "Siemen", "Tim"]]), expected)
     }
+
+    func testContainsTag() throws {
+        let template = """
+        #if(contains(core, "Tanner")):
+            Tanner is in the core team!
+        #endif
+        """
+
+        let expected = """
+        Tanner is in the core team!
+        """
+        try XCTAssertEqual(render(template, ["core": ["Tanner", "Logan", "Gwynne", "Siemen", "Tim"]]), expected)
+    }
+
+    func testDate() throws {
+        let template = """
+        The date is #date(now)
+        """
+
+        let expected = """
+        The date is 2020-11-09T14:30:00Z
+        """
+        try XCTAssertEqual(render(template, ["now": 1604932200]), expected)
+    }
+
+    func testDateWithCustomFormat() throws {
+        let template = """
+        The date is #date(now, "yyyy-MM-dd")
+        """
+
+        let expected = """
+        The date is 2020-11-09
+        """
+        try XCTAssertEqual(render(template, ["now": 1604932200]), expected)
+    }
 }
