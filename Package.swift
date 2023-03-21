41751003f9d1329bf0068b31ace9022dcec1b29c
Gwynne Raskind <gwynne@vapor.codes>
2023-03-21 16:45:01 -0500

    Add missing platform specifiers

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 let package = Package(
     name: "leaf-kit",
     platforms: [
-       .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
     ],
     products: [
         .library(name: "LeafKit", targets: ["LeafKit"]),
